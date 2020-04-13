e681def5573193a97a5a0af4ae19fa655a853343
Felix Anducho <felix.anducho74@gmail.com>
2020-04-13 14:10:48 -0500

    Add 'formatSearchInput' function to clean up the code a little bit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 
-
 const PrintListOfCountries = ({name, warning, index, getOne}) =>{
-
   if(warning){
     return <h1>{warning}</h1>
   } else {
@@ -39,13 +37,14 @@ const App = () => {
   const [newSearch, setNewSearch] = useState('');
   const [selectedCountry, setSelectedCountry] = useState([]);
 
+
   const handleSearchBarChange = event =>{
     setNewSearch(event.target.value);
     setSelectedCountry([])
   }
 
   const handleShowClick = (event) =>{
-    let name = event.target.parentNode.innerText.replace('Show','')
+    const name = event.target.parentNode.innerText.replace('Show','')
     axios
     .get(`https://restcountries.eu/rest/v2/name/${name}`)
     .then(response => {
@@ -53,27 +52,30 @@ const App = () => {
     })
   }
 
-  const countryToSearch = newSearch;
+  const formatSearchInput =  () =>{
+    const countryToSearch = newSearch;
+    const copyOfContries = countries;
+    let countryToSearchToUpper = '';
+  
+    //Make the first letter of the search query uppercase
+    if(countryToSearch !== ''){
+      countryToSearchToUpper = countryToSearch.charAt(0).toUpperCase() + countryToSearch.substring(1);
+    }
 
-  const copyOfContries = countries;
+    let  searchBarInput = copyOfContries.filter(el => el.name.includes(countryToSearchToUpper));
+    //Set the countries array to an empty array to prevent to print all
+    //the contries at once
+    if(countryToSearch === ''){
+      searchBarInput = [];
+    }
 
-  let countryToSearchToUpper = '';
+    if(searchBarInput.length > 10){
+      searchBarInput = [{ warning: 'Woops, too many matches. Be more specific'}]
+    }   
 
-  //Make the first letter of the search query uppercase
-  if(countryToSearch !== ''){
-    countryToSearchToUpper = countryToSearch.charAt(0).toUpperCase() + countryToSearch.substring(1);
+  return searchBarInput
   }
 
-  let searchBar = copyOfContries.filter(el => el.name.includes(countryToSearchToUpper));
-  //Set the countries array to an empty array to prevent to print all
-  //the contries at once
-  if(countryToSearch === ''){
-    searchBar = [];
-  }
-
-  if(searchBar.length > 10){
-    searchBar = [{ warning: 'Woops, too many matches. Be more specific'}]
-  }
 
   useEffect(() => {
     axios
@@ -85,25 +87,28 @@ const App = () => {
   },[])
 
   const ViewControl = ({selected}) =>{
+    
+    let searchBar = formatSearchInput();
+
     if(selected.length === 1){
       return selectedCountry.map(country => 
         <PrintCountry 
-        name={country.name} 
-        capital={country.capital} 
-        population={country.population} 
-        flag={country.flag}
-        language={country.languages}>
+          name={country.name} 
+          capital={country.capital} 
+          population={country.population} 
+          flag={country.flag}
+          language={country.languages}>
         </PrintCountry>
       )    
     } 
-    return searchBar.map((country, index) =>    
-    <PrintListOfCountries
-    name={country.name} 
-    key={index}
-    warning={country.warning}
-    getOne={handleShowClick}>
-    </PrintListOfCountries>)
 
+    return searchBar.map((country, index) => 
+    <PrintListOfCountries
+      name={country.name} 
+      key={index}
+      warning={country.warning}
+      getOne={handleShowClick}>
+    </PrintListOfCountries>)
   }
 
   return ( 
